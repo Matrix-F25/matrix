ec8688c60dea824f6036bc2131e1941a36f9ab86
Connor McRae <cjmcrae@ualberta.ca>
2025-09-25 19:47:39 -0600

    BLUEBLUEBLUE

--- app/src/main/java/com/example/matrix/Shape.java
+++ app/src/main/java/com/example/matrix/Shape.java
@@ -4,10 +4,6 @@ public class Shape {
     int x;
     int y;
 
-<<<<<<< HEAD
     String color = "blue";
 }
-=======
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
-}
->>>>>>> de0cc1a3237cf793533c88c8095bef8bdff4d4a0
+
